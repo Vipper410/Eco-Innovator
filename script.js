6a866aacd04ef0f2678f8d22d67cb569a09ee8ca
subhrakanta <senapatisubhra88@gmail.com>
2025-03-07 20:57:07 +0530

    script.js add

---
+++ script.js
@@ -0,0 +1,21 @@
+var ctx = document.getElementById('lineChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets: [{
+            label: 'Usage',
+            data: [850, 1010, 1350, 1500, 2000, 2200, 1750, 1600, 2100, 3000, 2440, 991],
+            backgroundColor: [
+                'rgba(85,85,85, 1)'
+
+            ],
+            borderColor: 'rgb(41, 155, 99)',
+
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
